974f6a820c45d4cff29f1faf15066ab3cf5c3ef1
Thomas Lee <tom@tleef.com>
2024-03-26 13:22:51 -0300

    Add simple conform definitions for maps and tuples

--- lib/spec.ex
+++ lib/spec.ex
@@ -27,6 +27,17 @@ end
 defimpl Specx.Spec, for: Tuple do
   def conform({:map, key_specs, _opts}, x) do
     if not is_map(x), do: throw(:invalid)
+
+    Map.merge(
+      x,
+      Enum.reduce(key_specs, %{}, fn spec, result ->
+        Specx.conform(spec, Map.get(x, resolve_key(spec)))
+        |> case do
+          :invalid -> throw(:invalid)
+          val -> Map.put(result, resolve_key(spec), val)
+        end
+      end)
+    )
   catch
     :invalid -> :invalid
   end
@@ -47,7 +58,24 @@ defimpl Specx.Spec, for: Tuple do
 
   def conform({:tuple, elem_specs, _opts}, x) do
     if not is_tuple(x), do: throw(:invalid)
+    if elem_specs != [] and length(elem_specs) != tuple_size(x), do: throw(:invalid)
+
+    Enum.with_index(elem_specs)
+    |> Enum.reduce({}, fn {spec, index}, result ->
+      Specx.conform(spec, elem(x, index))
+      |> case do
+        :invalid -> throw(:invalid)
+        el -> Tuple.insert_at(result, index, el)
+      end
+    end)
   catch
     :invalid -> :invalid
+  end
+
+  defp resolve_key(spec) when is_atom(spec) do
+    Atom.to_string(spec)
+    |> String.split("/")
+    |> List.last()
+    |> String.to_existing_atom()
   end
 end
